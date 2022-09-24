ceb57befb6e25c272757e03551fa19b8b81ebf65
satinder <satindersingh472@gmail.com>
2022-09-24 13:29:29 -0600

    updated

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('hello world!!!')
+import wikipedia
+
+def ask_user():
+    user_input = input("Enter any city name of Canada:  ")
+    try:
+        user_input = wikipedia.page(user_input)
+        print(user_input.title)
+        print(user_input.content)
+    except:
+        print('Not a valid entry, page does not exists')
+    finally:
+        print('thanks for using wikipedia')
+
+ask_user()
