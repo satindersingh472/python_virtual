991cbb8ef01f7543426d550a8acdf957f9889445
satinder <satindersingh472@gmail.com>
2022-09-24 14:03:01 -0600

    finished

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import wikipedia
-
+import webbrowser
 def ask_user():
     user_input = input("Enter any city name of Canada:  ")
     try:
+        wikipedia.set_lang('en')
         user_input = wikipedia.page(user_input)
         print(user_input.title)
         print(user_input.content)
+        webbrowser.open(user_input.url)
+    except wikipedia.exceptions.PageError:
+        print('This page does not exists')
     except:
-        print('Not a valid entry, page does not exists')
+        print('Sorry, something went wrong')
     finally:
         print('thanks for using wikipedia')
 
